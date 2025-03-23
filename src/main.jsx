ae0f5efc4924255d1a17f27e246556f906554695
Ushio12345 <anhqueht1100@gmail.com>
2025-03-24 01:29:29 +0700

    fix deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.js";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "./styles/GlobalStyles.scss";
 import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-            <BrowserRouter>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </PersistGate>
     </Provider>
 );
